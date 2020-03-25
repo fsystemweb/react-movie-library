f30f3061d6f5dd0bab4efdc81734973463538e20
Facundo Ezequiel Diaz <contacto@fsystem.com.ar>
2020-03-25 14:53:55 +0100

    movie component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bulma/css/bulma.css";
 
 import { Title } from "./components/Title";
 import { SearchForm } from "./components/SearchForm";
+import { MovieList } from "./components/MovieList";
 
 class App extends Component {
   state = { results: [] };
@@ -11,14 +12,6 @@ class App extends Component {
   _handleResults = results => {
     this.setState({ results });
   };
-
-  _renderResults() {
-    const { results } = this.state;
-
-    return results.map(movie => {
-      return <p key={movie.imdbID}>{movie.Title}</p>;
-    });
-  }
 
   render() {
     return (
@@ -30,7 +23,7 @@ class App extends Component {
         {this.state.results.length === 0 ? (
           <p>Sin resultados</p>
         ) : (
-          this._renderResults()
+          <MovieList movies={this.state.results} />
         )}
       </div>
     );
